3479a08f0d96f838d064200196d8dece7ac3ed27
DESKTOP-06NQLD2\82106 <codo47@naver.com>
2022-01-28 00:19:21 +0900

    common-module

--- js/main.js
+++ js/main.js
@@ -1,26 +1,3 @@
-const searchEl=document.querySelector('.search');
-const searchInputEl=searchEl.querySelector('input');
-
-
-searchEl.addEventListener('click', function (){
-    searchInputEl.focus();
-});
-
-
-searchInputEl.addEventListener('focus', function(){
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder','통합검색');
-
-
-});
-
-searchInputEl.addEventListener('blur', function(){
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder','');
-
-
-});
-
 const badgeEl=document.querySelector('header .badges');
 const toTopEl =document.querySelector('#to-top');
 window.addEventListener('scroll', _.throttle(function(){
@@ -153,7 +130,3 @@ spyEls.forEach(function(spyEl){
         .addTo(new ScrollMagic.Controller());
 
 });
-
-
-const thisYear=document.querySelector('.this-year');
-thisYear.textContent=new Date().getFullYear();
